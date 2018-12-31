c65dfcfb12a4f71f767839462bffac231893e8cd
“purplesunset871019” <hsb41202@gmail.com>
2018-12-31 15:42:19 +0800

    Quick Sort second

--- src/com/data/third/Quick_Sort.java
+++ src/com/data/third/Quick_Sort.java
@@ -1,59 +1,78 @@
 package com.data.third;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Quick_Sort {
 	
-	static int [] num = {41,24,76,11,45,64,21,69,19,36};
+	static int []num = new int[10];;
+	static int times=1;
 	
     public static void main(String [] argv){
-        quick_sort();
+    	
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("請輸入選取數字上限：");
+		int n = scanner.nextInt();
+    	
+    	
+    	
+    	Random rand = new Random();
+    	
+		for (int i=0; i<10; i++){
+			num[i] = rand.nextInt(n)+1;		// 將隨機數(1-49)放入 sixNum[i]
+			for (int j=0; j<i;){			// 與前數列比較，若有相同則再取亂數
+				if (num[j]==num[i]){	
+					num[i] = rand.nextInt(n)+1;
+					j=0;			// 避面重新亂數後又產生相同數字，若出現重覆，迴圈從頭開始重新比較所有數
+				}
+				else j++;			// 若都不重複則下一個數
+			}
+		}
+    	
+    	
+    	
+    	System.out.print("原始資料：");
+        for(int i : num){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        
+    	quick_sort();
+    	System.out.print("排序結果：");
         for(int i : num){
             System.out.print(i + " ");
         }
     }
-    //Quick Sort Start
+    
     public static void quick_sort(){
         sort(0,num.length-1);
     }
-    //Recursion
+    
     public static void sort(int left, int right){
         //確定是否可以排序
+    	
     	if(left < right){
-    		int i;
-            int j;
-            
-            for(int a=1;;a++) {
-                for(i=left;i<num.length;i++) {
-                	if(num[i]>num[left]) {
-                		break;
-                	}
-                }
-                
-                for(j=right;j>0;j--) {
-                	if(num[j]<num[left]) {
-                		break;
-                	}
-                }
-                
-                if(i<j) {
-                	swap(i,j);
-                }
-                
-                if(i>=j) {
-                	break;
-                }
-            }
-            
 
-            
-            /*while(true){
+    		int i = left;
+            int j = right+1;
+    		
+            while(true){
                 //向右找，直到找到比基準點大的
-                while(i + 1 < num.length && num[++i] < num[left]);
+                while(i + 1 < num.length && num[++i] < num[left]) {
+                	
+                };
+                
                 //向左找，直到找到比基準點小的
-                while(j - 1 >= 0 && num[--j] > num[left]);
-                if(i >= j) break;
+                while(j - 1 >= 0 && num[--j] > num[left]) {
+                	
+                };
+                
+                if(i >= j)
+                	break;
+                
                 swap(i,j);
-            }*/
-            //基準點與j交換(這時候的j已經跑到比基準點小的數了)
+            }
+            //基準點與j交換(這時候的j已經跑到比基準點小的數了)            
             swap(left,j);
             //遞迴排序基準點左半邊
             sort(left, j - 1);
@@ -63,23 +82,24 @@ public class Quick_Sort {
     }
     //交換
     public static void swap(int i, int j){
-    	if(i>j &&num[i]>num[j]) {
-    	}
+    	int temp = num[i];
+        num[i] = num[j];
+        num[j] = temp;
+        
+        if(times<10) {
+            System.out.print("處理過程"+times+" ：");
+            times++;
+        }
+        else {
+            System.out.print("處理過程"+times+"：");
+            times++;
+        }
+
+        for(int z = 0; z < num.length; z++) {
+        	System.out.print("["+num[z]+"]"+" ");
+        }
+        System.out.println();
     	
-    	else {
-            int temp = num[i];
-            int text = num[j];
-            for(int z = 0; z < num.length; z++) {
-                System.out.print(num[z]+" ");
-            }
-            System.out.println();
-            num[i] = num[j];
-            num[j] = temp;
-            for(int z = 0; z < num.length; z++) {
-            	System.out.print(num[z]+" ");
-            }
-            System.out.println();
-    	}
     }
 
 }
